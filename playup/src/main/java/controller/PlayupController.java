04bf9b7c2246ef7fd41391adf5b08e79785c036d
Rajath Shashidhar Bharadwaj <rj839212@dal.ca>
2021-10-16 16:08:01 -0300

    base code

---
+++ playup/src/main/java/controller/PlayupController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayupController {
+    @GetMapping
+    public String BaseCode(){
+        return ("Hello World");
+    }
+}
---
+++ playup/src/test/java/com/playup/playup/PlayupApplicationTests.java
@@ -0,0 +1,8 @@
+package com.playup.playup;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayupApplicationTests {
+
+}
