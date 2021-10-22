73e41dac6914221414f0be1e24840bd67205edad
vibhor-bhatnagar <81545719+vibhor-bhatnagar@users.noreply.github.com>
2021-10-22 17:53:07 -0300

    Added test class for controller and DB connection

--- playup/src/test/java/com/playup/playup/PlayupApplicationTests.java
+++ playup/src/test/java/com/playup/playup/PlayupApplicationTests.java
@@ -1,8 +1,26 @@
 package com.playup.playup;
 
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.playup.playup.controller.PlayupController;
+import com.playup.playup.controller.PlayupDBConnection;
 
 @SpringBootTest
 class PlayupApplicationTests {
-
+	
+	@Test
+	public void controllerTest() {
+		PlayupController testController = new PlayupController();
+		assertTrue(testController.BaseCode().equals("Hello World"));
+	}
+	
+	@Test
+	public void PlayupDBConnectionTest() {
+		PlayupDBConnection connectTest = new PlayupDBConnection();
+		String actualOutput = connectTest.dbConnect();
+		assertEquals(actualOutput, "Vibhor");
+	}
 }
