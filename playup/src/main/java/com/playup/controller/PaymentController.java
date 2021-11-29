6fc2966b216e76a62fe43e418eca05dafea7b377
Shiv Gaurang Desai <sh721068@dal.ca>
2021-11-29 17:45:16 -0400

    changes in the html layout

--- playup/src/main/java/com/playup/controller/PaymentController.java
+++ playup/src/main/java/com/playup/controller/PaymentController.java
@@ -1,4 +1,17 @@
 package com.playup.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PaymentController {
+
+    @GetMapping("/Payment")
+    public ModelAndView paymentMethod() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("payment");
+        return mv;
+    }
+
 }
