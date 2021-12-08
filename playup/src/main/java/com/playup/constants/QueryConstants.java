52ffe3072bbc6a0460c6aa0bed842f10067b23d2
vibhor-bhatnagar <81545719+vibhor-bhatnagar@users.noreply.github.com>
2021-12-08 18:28:22 -0400

    Addition to Query constants

--- playup/src/main/java/com/playup/constants/QueryConstants.java
+++ playup/src/main/java/com/playup/constants/QueryConstants.java
@@ -8,4 +8,6 @@ public class QueryConstants {
     public final static String INSERT_SUPPORT_REQUEST_QUERY = "INSERT INTO Support (ticketNumber, name, email, venue, description) VALUES (%s, '%s', '%s', '%s', '%s');";
     public final static String CHECK_TRANSACTION_ID_NUMBER_QUERY = "SELECT * from Payment where transactionId = %s;";
     public final static String INSERT_TRANSACTION_QUERY = "INSERT INTO Payment (TransactionId, name, amount, timeStamp, cardNumber) VALUES (%s, '%s', '%s', '%s', '%s');";
+	public final static String INSERT_INTO_MATCHES_TABLE = "INSERT INTO Tournament (Tournament_id, matches_list) VALUES ('";
+	public final static String INSERT_INTO_TOURNAMENT_TABLE = "INSERT INTO Tournament (userId, tournament_id, sport_name, team_number) VALUES ('";
 }
