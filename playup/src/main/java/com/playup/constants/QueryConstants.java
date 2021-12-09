bce8024bd471c82306c07e679afc75711fe49796
Shiv Gaurang Desai <sh721068@dal.ca>
2021-12-09 16:45:55 -0400

    changing server endpoints

--- playup/src/main/java/com/playup/constants/QueryConstants.java
+++ playup/src/main/java/com/playup/constants/QueryConstants.java
@@ -5,6 +5,9 @@ package com.playup.constants;
  */
 
 public class QueryConstants {
+    public final static String DB_URL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_1_PRODUCTION";
+    public final static String DB_USERNAME = "CSCI5308_1_PRODUCTION_USER";
+    public final static String DB_PASSWORD = "Ahgheigoo3jaYohl";
     public final static String CHECK_SUPPORT_TICKET_NUMBER_QUERY = "SELECT * from Support where ticketNumber = %s;";
     public final static String INSERT_SUPPORT_REQUEST_QUERY = "INSERT INTO Support (ticketNumber, name, email, venue, description) VALUES (%s, '%s', '%s', '%s', '%s');";
     public final static String CHECK_TRANSACTION_ID_NUMBER_QUERY = "SELECT * from Payment where transactionId = %s;";
@@ -18,9 +21,6 @@ public class QueryConstants {
     public final static String PAYMENT_QUERY_EMAIL = "email";
     public final static String INSERT_INTO_MATCHES_TABLE = "INSERT INTO Tournament (Tournament_id, matches_list) VALUES ('";
     public final static String INSERT_INTO_TOURNAMENT_TABLE = "INSERT INTO Tournament_Details (userId, tournament_id, sport_name, team_number) VALUES ('";
-    public final static String PRODUCTION_DB_URL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_1_PRODUCTION";
-    public final static String PRODUCTION_DB_USERNAME = "CSCI5308_1_PRODUCTION_USER";
-    public final static String PRODUCTION_DB_PASSWORD = "Ahgheigoo3jaYohl";
     public final static String GET_VENUE_BY_ID = "Select * from Venues where venue_id=";
     public final static String GET_SLOT_BY_VENUE_ID = "Select * from venue_slot_mapping where venue_id=";
     public final static String GET_LOCATION_OF_ALL_VENUES = "Select venue_id, latitude, longitude from Venues";
