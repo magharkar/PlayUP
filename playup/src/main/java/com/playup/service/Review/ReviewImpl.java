71d1a88bd7f290e7a997ac3d608482dfd320ba09
Rajath Bharadwaj <rj839212@dal.ca>
2021-12-09 01:42:02 -0400

    Review Feature

---
+++ playup/src/main/java/com/playup/service/Review/ReviewImpl.java
@@ -0,0 +1,50 @@
+package com.playup.service.Review;
+
+import com.playup.dao.Review.ReviewDao;
+import com.playup.model.Review.ReviewModel;
+import com.playup.model.Review.ViewReviewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReviewImpl {
+
+    private static ReviewImpl instance = null;
+    public static ReviewImpl getInstance() {
+        if(instance == null) {
+            instance = new ReviewImpl();
+        }
+        return instance;
+    }
+
+    public ArrayList<ReviewModel> fetchVenues()
+    {
+        List<ReviewModel> venueList = new ArrayList<>();
+        try
+        {
+            venueList = ReviewDao.getInstance().getVenuesFromDB();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return (ArrayList<ReviewModel>) venueList;
+    }
+
+    public ArrayList<ViewReviewModel> viewReviews()
+    {
+        List<ViewReviewModel> viewReviewList = new ArrayList<>();
+        try
+        {
+            viewReviewList = ReviewDao.getInstance().getReviewsFromDB();
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return (ArrayList<ViewReviewModel>) viewReviewList;
+    }
+
+    public void postReviews(int venueID, String title, String description, int rating)
+    {
+       ReviewDao.getInstance().postReviewToDB( venueID,title,description,rating);
+    }
+}
