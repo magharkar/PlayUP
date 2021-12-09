e7763c22f65cdf6eedd6d278f8d489851b80d9f1
Shiv Gaurang Desai <sh721068@dal.ca>
2021-12-09 06:47:33 -0400

    changed in test cases

--- playup/src/test/java/com/playup/service/support/TicketGeneratorServiceTest.java
+++ playup/src/test/java/com/playup/service/support/TicketGeneratorServiceTest.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @author Shiv Gaurang Desai
  */
 public class TicketGeneratorServiceTest {
-    @Autowired
-    SupportTicketGeneratorServiceImpl ticketGeneratorService;
     @Test
     void ticketGeneratorServiceNotNullTest() throws ClassNotFoundException {
         Class<?> classFinder = Class.forName("com.playup.service.support.SupportTicketGeneratorServiceImpl", false, getClass().getClassLoader());
@@ -19,12 +17,14 @@ public class TicketGeneratorServiceTest {
 
     @Test
     void ticketNumberValidTest() {
+        SupportTicketGeneratorServiceImpl ticketGeneratorService = new SupportTicketGeneratorServiceImpl();
         int number = ticketGeneratorService.generateTicketNumber(ApplicationConstants.MINIMUM_SUPPORT_TICKET_NUMBER,ApplicationConstants.MAXIMUM_SUPPORT_TICKET_NUMBER);
         assertTrue(number>=1000&&number<=10000);
     }
 
     @Test
     void ticketNumberNotValidTest() {
+        SupportTicketGeneratorServiceImpl ticketGeneratorService = new SupportTicketGeneratorServiceImpl();
         int number = ticketGeneratorService.generateTicketNumber(ApplicationConstants.MINIMUM_SUPPORT_TICKET_NUMBER,ApplicationConstants.MAXIMUM_SUPPORT_TICKET_NUMBER);
         assertFalse(number<1000||number>10000);
     }
