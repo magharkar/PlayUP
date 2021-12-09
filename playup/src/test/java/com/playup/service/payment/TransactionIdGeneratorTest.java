6fccd7b1cdd2ce6ac756faed6e97745486e9bd01
Shiv Gaurang Desai <sh721068@dal.ca>
2021-12-09 06:13:59 -0400

    changing in the test cases

--- playup/src/test/java/com/playup/service/payment/TransactionIdGeneratorTest.java
+++ playup/src/test/java/com/playup/service/payment/TransactionIdGeneratorTest.java
@@ -2,12 +2,17 @@ package com.playup.service.payment;
 
 import com.playup.constants.ApplicationConstants;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Shiv Gaurang Desai
  */
 public class TransactionIdGeneratorTest {
+    @Autowired
+    ITransactionIdGeneratorService TransactionIdGeneratorService;
+
     @Test
         void TransactionIdGeneratorServiceNotNullTest() throws ClassNotFoundException {
         Class<?> classFinder = Class.forName("com.playup.service.payment.TransactionIdGeneratorServiceImpl", false, getClass().getClassLoader());
@@ -16,14 +21,12 @@ public class TransactionIdGeneratorTest {
 
     @Test
     void TransactionIdNumberValidTest() {
-        ITransactionIdGeneratorService TransactionIdGeneratorService = new TransactionIdGeneratorServiceImpl();
         int number = TransactionIdGeneratorService.generateTransactionId(ApplicationConstants.MINIMUM_TRANSACTION_NUMBER,ApplicationConstants.MAXIMUM_TRANSACTION_NUMBER);
         assertTrue(number>=1000&&number<=10000);
     }
 
     @Test
     void TransactionIdNotValidTest() {
-        ITransactionIdGeneratorService TransactionIdGeneratorService = new TransactionIdGeneratorServiceImpl();
         int number = TransactionIdGeneratorService.generateTransactionId(ApplicationConstants.MINIMUM_TRANSACTION_NUMBER,ApplicationConstants.MAXIMUM_TRANSACTION_NUMBER);
         assertFalse(number<1000||number>10000);
     }
